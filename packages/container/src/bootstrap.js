621d048d29155b1c861f38078790f3f9429c0995
surbhi-p <surbhip561@gmail.com>
2024-09-07 21:59:38 +0530

    refinement

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-/**
- * render container app
- */
+
 ReactDOM.render(<App />, document.querySelector('#root'));
