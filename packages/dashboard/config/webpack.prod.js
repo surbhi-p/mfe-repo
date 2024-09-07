c93680c146aba2b5c83ca70ad39523b4357bbae6
surbhi-p <surbhip561@gmail.com>
2024-09-07 23:42:39 +0530

    changes

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -1,15 +1,13 @@
 const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
-const domain = process.env.PRODUCTION_DOMAIN
-
-const pordConfig = {
+const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/dashboard/latest',
+    publicPath: '/dashboard/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -18,9 +16,9 @@ const pordConfig = {
       exposes: {
         './DashboardApp': './src/bootstrap',
       },
-      shared: packageJson.dependencies
-    })
-  ]
-}
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
-module.exports = merge(commonConfig, pordConfig);
+module.exports = merge(commonConfig, prodConfig);
