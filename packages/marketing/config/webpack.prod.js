0991c7ec071ef41650a2ea6964cb0e9c1e64e0b1
surbhi-p <surbhip561@gmail.com>
2024-09-07 23:04:07 +0530

    deploy marketing

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -3,12 +3,11 @@ const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN
-
 const pordConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
