3fd6c1523c76d0c64ebc66dcce7556f6bdaf5a54
surbhi-p <surbhip561@gmail.com>
2024-09-07 20:04:59 +0530

    add comment

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+/**
+ * render container app
+ */
 ReactDOM.render(<App />, document.querySelector('#root'));
